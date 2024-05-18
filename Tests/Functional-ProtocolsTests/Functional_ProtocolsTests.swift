97a54e0198e1208b821e6719689797e805051abe
Christoph <christoph_rohde@icloud.com>
2024-05-18 13:40:36 +0200

    Initial Commit

---
+++ Tests/Functional-ProtocolsTests/Functional_ProtocolsTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import Functional_Protocols
+
+final class Functional_ProtocolsTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
